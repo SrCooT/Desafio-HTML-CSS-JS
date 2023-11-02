dc1240c8aaeb4985b55f120460e272a1959d947c
Juno Crisitan <juno.lp@hotmail.com>
2023-11-02 13:18:14 -0300

    vaaai

--- src/js/form.js
+++ src/js/form.js
@@ -5,15 +5,15 @@ const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    nameValidate();
-    emailValidate();
-    numberValidate();
-    menssageValidate();
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     nameValidate();
+//     emailValidate();
+//     numberValidate();
+//     menssageValidate();
 
 
-});
+// });
 
 function spanError(index) {
     campos[index].style.border = '2px solid #F52E2E'
@@ -23,36 +23,36 @@ function removeError(index) {
     campos[index].style.border = '2px solid #00C22B';
     span[index].style.display = 'none';
 }
-function nameValidate() {
-    if (campos[0].value.length < 3) {
-        spanError(0);
-    } else {
-        removeError(0);
-    };
-};
+// function nameValidate() {
+//     if (campos[0].value.length < 3) {
+//         spanError(0);
+//     } else {
+//         removeError(0);
+//     };
+// };
 
-function numberValidate() {
-    if (campos[2].value.length < 8) {
-        spanError(2);
-    } else {
-        removeError(2);
-    };
-};
+// function numberValidate() {
+//     if (campos[2].value.length < 8) {
+//         spanError(2);
+//     } else {
+//         removeError(2);
+//     };
+// };
 
-function emailValidate() {
-    if (!emailRegex.test(campos[1].value)) {
-        spanError(1);
-    } else {
-        removeError(1);
-    };
-};
-function menssageValidate() {
-    if (campos[3].value.length < 15) {
-        spanError(3);
-    } else {
-        removeError(3);
-    };
-};
+// function emailValidate() {
+//     if (!emailRegex.test(campos[1].value)) {
+//         spanError(1);
+//     } else {
+//         removeError(1);
+//     };
+// };
+// function menssageValidate() {
+//     if (campos[3].value.length < 15) {
+//         spanError(3);
+//     } else {
+//         removeError(3);
+//     };
+// };
 
 
 
